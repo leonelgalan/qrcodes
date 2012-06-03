a14d8d6ce1a8b6b961bc29e629c8e1d6b7597d3c
Leonel Galan <leonel@smashingboxes.com>
2012-06-03 14:46:16 -0400

    Stores and compose QR codes and short urls

--- Gemfile
+++ Gemfile
@@ -5,4 +5,16 @@ gem 'thin', '1.3.1'
 
 gem 'rqrcode', '0.4.2'
 gem 'chunky_png', '1.2.5'
-gem 'barby', git:'git://github.com/3kwa/barby.git'
+gem 'barby', git: 'git://github.com/3kwa/barby.git'
+gem 'datamapper', '1.2.0'
+gem 'rmagick', '2.13.1', require: 'RMagick'
+
+group :production do
+    gem 'pg'
+    gem 'dm-postgres-adapter'
+end
+
+group :development, :test do
+    gem 'sqlite3'
+    gem 'dm-sqlite-adapter'
+end
--- app.rb
+++ app.rb
@@ -2,6 +2,20 @@ require 'sinatra'
 require 'barby'
 require 'barby/barcode/qr_code'
 require 'barby/outputter/png_outputter'
+require 'data_mapper'
+require 'RMagick'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/development.db")
+
+class Link
+  include DataMapper::Resource
+  property :short_url, String, :key => true
+  property :url, String
+  property :title, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 class String
   def titleize
@@ -13,6 +27,20 @@ helpers do
   def checkbox(name, value, label=value.titleize)
     "<input type=\"radio\" name=\"#{name}\" id=\"#{name}_#{value}\" value=\"#{value}\" /> <label for=\"#{name}_#{value}\">#{label}</label><br />"
   end
+  
+  def random_string
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    chars.sample + chars.sample + chars.sample
+  end
+
+  def gen_short_url
+    tmp = random_string
+    until Link.get(tmp).nil?
+      tmp = random_string
+    end
+
+    tmp
+  end
 end
 
 get '/' do
@@ -20,22 +48,62 @@ get '/' do
 end
 
 get '/generate' do
-  @qr_code_url = "/qr?url=#{params[:url]}&color=#{params[:color]}"
-  erb :generate
+  @qr_code_url = "/qr?url=#{params[:url]}&color=#{params[:color]}&title=#{params[:title]}"
+  @link = Link.new short_url: gen_short_url, url: params[:url], title: params[:title]
+
+  if @link.save
+    erb :generate
+  else
+    status 400
+    erb :index
+  end
 end
 
 get '/qr' do
   color = case params[:color]
   when 'red'
-    '#FF0000'
+    '#BD0A0A'
   when 'green'
-    '#00FF00'
+    '#267519'
   when 'blue'
-    '#0000FF'
+    '#134EAF'
+  when 'brown'
+    '#4B2E05'
+  when 'purple'
+    '#340645'
   else
     '#000000'
   end
 
+  qr_code = Barby::QrCode.new(params[:url], level: :h)
+  File.open("tmp.png", 'w'){|f|
+    f.write qr_code.to_png(:margin => 0, :height => 300, :width => 300, xdim: 10, :color => ChunkyPNG::Color.from_hex(color), :bgcolor => ChunkyPNG::Color::WHITE)
+  }
+  
+  src = Magick::Image.read("tmp.png").first
+  dst = Magick::Image.read("images/blue.png").first
+  result = dst.composite(src, Magick::CenterGravity, Magick::OverCompositeOp)
+  
+  gc = Magick::Draw.new
+  gc.fill = 'white'
+  gc.stroke = 'none'
+  gc.pointsize = 42
+
+  text = Magick::Draw.new
+  text.font = 'DINPro-Medium.otf'
+  text.pointsize = 52
+  text.gravity = Magick::CenterGravity
+  
+  text.annotate(result, 0,0,0,300, params[:title]){
+    self.fill = 'white'
+  }
+  
   headers 'Content-Type' => 'img/png'
-  Barby::QrCode.new(params[:url], level: :h).to_png(:margin => 0, :height => 300, :width => 300, xdim: 10, :color => ChunkyPNG::Color.from_hex(color), :bgcolor => ChunkyPNG::Color::TRANSPARENT)
+  result.to_blob { self.format = "PNG" }
+  #result.write('composite1.gif')
+end
+
+get '/:key' do
+  @link = Link.get(params[:key])
+  redirect @link.url
 end
