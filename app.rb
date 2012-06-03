f14a953af3d016f09c8abecb18e88ee2319d5904
Leonel Galan <leonel@smashingboxes.com>
2012-06-02 23:34:40 -0400

    QR Code generation using Google Charts API

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
 get '/' do
-  ''
+  'Visit /generate?url= to generate a QR Code for the specified URL'
+end
+
+get '/generate' do
+  @qr_code_url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=#{params[:url]}&chld=H|0"
+  erb :generate
 end
