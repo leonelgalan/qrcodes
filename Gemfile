f1a854df7f73017449b5b7d5dcc39a69cefe25fb
Leonel Galan <leonel@smashingboxes.com>
2012-06-03 02:19:02 -0400

    Self generated QR codes and color

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source :rubygems
 
 gem 'sinatra', '1.3.2'
 gem 'thin', '1.3.1'
+
+gem 'rqrcode', '0.4.2'
+gem 'chunky_png', '1.2.5'
+gem 'barby', git:'git://github.com/3kwa/barby.git'
--- app.rb
+++ app.rb
@@ -1,10 +1,29 @@
 require 'sinatra'
+require 'barby'
+require 'barby/barcode/qr_code'
+require 'barby/outputter/png_outputter'
 
 get '/' do
   erb :index
 end
 
 get '/generate' do
-  @qr_code_url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=#{params[:url]}&chld=H|0"
+  @qr_code_url = "/qr?url=#{params[:url]}&color=#{params[:color]}"
   erb :generate
+end
+
+get '/qr' do
+  color = case params[:color]
+  when 'red'
+    '#FF0000'
+  when 'green'
+    '#00FF00'
+  when 'blue'
+    '#0000FF'
+  else
+    '#000000'
+  end
+
+  headers 'Content-Type' => 'img/png'
+  Barby::QrCode.new(params[:url], level: :h).to_png(:margin => 0, :height => 300, :width => 300, xdim: 10, :color => ChunkyPNG::Color.from_hex(color), :bgcolor => ChunkyPNG::Color::TRANSPARENT)
 end
